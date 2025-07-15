34cb020868c8881f8ddc0b2bb8666631182b99fa
RenanAugustoKwn <renanaugustokwn@outlook.com>
2025-07-15 15:45:34 -0300

    Botoes Controles

--- Assets/Scripts/PlayerStacker.cs
+++ Assets/Scripts/PlayerStacker.cs
@@ -3,22 +3,58 @@ using System.Collections.Generic;
 
 public class PlayerStacker : MonoBehaviour
 {
+    [Header("Camadas")]
+    public LayerMask greenGroundMask;   // atribua “GreenZone” no Inspector
+    public float greenGroundCheckRadius = 0.9f;
+
     [Header("Referência")]
     public Transform stackPoint;            // objeto vazio nas costas
     public GameController gameController;
+    public GameObject[] iconsImage;
 
     [Header("Configuração")]
     public float stackSpacingY = 1.2f;
     public float pickupRange = 2f;
 
     private readonly List<EnemyRagdoll> stacked = new();
+    private bool isInGreenGround = false;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T)) TryPickupEnemy();
-        if (Input.GetKeyDown(KeyCode.Y)) DropEnemy();
+        if (Input.GetKeyDown(KeyCode.T) || Input.GetKeyDown(KeyCode.JoystickButton2)) TryPickupEnemy();
+        if (Input.GetKeyDown(KeyCode.Y) || Input.GetKeyDown(KeyCode.JoystickButton1)) DropEnemy();
+        isInGreenGround = Physics.CheckSphere(transform.position, greenGroundCheckRadius, greenGroundMask);
+        UpdatePickupIcon();
     }
+    /* ---------- Mostrar / esconder ícone ---------- */
+    void UpdatePickupIcon()
+    {
+        if (iconsImage.Length < 2) return;
 
+        if (isInGreenGround)
+        {
+            iconsImage[0].SetActive(false);  // Desativa X
+            iconsImage[1].SetActive(true);   // Ativa B
+        }
+        else
+        {
+            bool canPickup = false;
+
+            Collider[] hits = Physics.OverlapSphere(transform.position, pickupRange);
+            foreach (var h in hits)
+            {
+                EnemyRagdoll rag = h.GetComponentInParent<EnemyRagdoll>();
+                if (rag != null && rag.isRagdolled && !rag.isStacked && rag.pickupBool)
+                {
+                    canPickup = true;
+                    break;
+                }
+            }
+
+            iconsImage[0].SetActive(canPickup); // Ativa X se pode pegar
+            iconsImage[1].SetActive(false);     // Desativa B fora da área
+        }
+    }
     /* ---------- Coletar ---------- */
     void TryPickupEnemy()
     {
